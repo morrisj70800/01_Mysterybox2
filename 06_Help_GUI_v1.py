67c997ba421c8ce5f118469e2723d788c138cf9e
morrisj70800 <78512092+morrisj70800@users.noreply.github.com>
2021-03-25 12:19:12 +1300

    HELP GUI

---
+++ 06_Help_GUI_v1.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from functools import partial   # To prevent unwanted windows
+import random
+
+
+class Start:
+    def __init__(self, parent):
+
+        # GUI to get starting balance and stakes
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Mystery Heading (row 0)
+        self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game",
+                                       font="Arial 19 bold")
+        self.mystery_box_label.grid(row=1)
+
+        # Help Button (row 2)
+        self.help_button = Button(self.start_frame, text="Help",
+                                  command=self.to_help)
+        self.help_button.grid(row=2, pady=10)
+
+    def to_help(self):
+        get_help = Help(self)
+
+
+class Help:
+    def __init__(self, partner):
+
+        # disable help button
+        partner.help_button.config(state=DISABLED)
+
+        # Sets up child window (ie: help box)
+        self.help_box = Toplevel()
+
+        # If users press cross at top, closes help and 'releaes' help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+
+        # Set up GUI Frame
+        self.help_frame
